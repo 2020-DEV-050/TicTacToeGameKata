f14cd18101e98b4a59895d8cc66da033f2cdc11a
2020-DEV-050 <2020dev050@gmail.com>
2020-03-03 12:28:48 +0530

    Refactored: Renamed constant in GameBoardTest to make it more meaningful

--- src/test/java/com/bnpp/kata/GameBoardTest.java
+++ src/test/java/com/bnpp/kata/GameBoardTest.java
@@ -7,15 +7,15 @@ import org.junit.Test;
 
 public class GameBoardTest {
 
-	private static final char PLAYER_X = 'X';
+	private static final char SYMBOL_X = 'X';
 	private static final int ZERO = 0;
 
 	@Test
 	public void playerOneShouldBeAbleToPlaceXInAnyCell() {
 		GameBoard board = new GameBoard();
 
-		board.drawSymbolAt(ZERO, ZERO, PLAYER_X);
+		board.drawSymbolAt(ZERO, ZERO, SYMBOL_X);
 
-		assertThat(board.getSymbolAt(ZERO, ZERO), CoreMatchers.is(PLAYER_X));
+		assertThat(board.getSymbolAt(ZERO, ZERO), CoreMatchers.is(SYMBOL_X));
 	}
 }
