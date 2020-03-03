169f8c28ac6b0695f9c1cb7b74eb57dce46ef74e
2020-DEV-050 <2020dev050@gmail.com>
2020-03-03 13:02:54 +0530

    Refactored: Modified the object initialization for easy reusability

--- src/test/java/com/bnpp/kata/TicTacToeGameTest.java
+++ src/test/java/com/bnpp/kata/TicTacToeGameTest.java
@@ -1,8 +1,9 @@
 package com.bnpp.kata;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TicTacToeGameTest {
@@ -10,9 +11,15 @@ public class TicTacToeGameTest {
 	private static final char PLAYER_X = 'X';
 	private static final int ZERO = 0;
 
+	TicTacToeGame game;
+
+	@Before
+	public void init() {
+		game = new TicTacToeGame();
+	}
+
 	@Test
 	public void playerOneShouldBeAbleToPlaceXInAnyCell() throws CellAlreadyOccupiedException {
-		TicTacToeGame game = new TicTacToeGame();
 
 		game.play(ZERO, ZERO);
 
@@ -21,7 +28,6 @@ public class TicTacToeGameTest {
 
 	@Test(expected = CellAlreadyOccupiedException.class)
 	public void shouldThroughExceptionWhenPlayerTryToFillInNonEmptyCell() throws CellAlreadyOccupiedException {
-		TicTacToeGame game = new TicTacToeGame();
 
 		game.play(ZERO, ZERO);
 		game.play(ZERO, ZERO);
