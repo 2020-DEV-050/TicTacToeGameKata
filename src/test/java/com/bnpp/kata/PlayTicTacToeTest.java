097fa6fc7bae3a7fdd1ad9e6c375626da26880d4
2020-DEV-050 <2020dev050@gmail.com>
2020-03-03 20:37:51 +0530

    Verified: PlayTicTacToe instance should not be null

---
+++ src/test/java/com/bnpp/kata/PlayTicTacToeTest.java
@@ -0,0 +1,16 @@
+package com.bnpp.kata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayTicTacToeTest {
+
+	@Test
+	public void playTicTacToeInstanceShouldNotBeNull() {
+		
+		PlayTicTacToe playTicTacToe = new PlayTicTacToe();
+		assertNotNull(playTicTacToe);
+	}
+
+}
