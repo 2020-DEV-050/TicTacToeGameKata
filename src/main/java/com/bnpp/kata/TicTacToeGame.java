f920ef207a2b139e9f9cc978bf46bba065e210d4
2020-DEV-050 <2020dev050@gmail.com>
2020-03-03 12:02:25 +0530

    Refactored: Extracted constants and added no argument constructor to initialize the game board array

--- src/main/java/com/bnpp/kata/TicTacToeGame.java
+++ src/main/java/com/bnpp/kata/TicTacToeGame.java
@@ -2,13 +2,18 @@ package com.bnpp.kata;
 
 public class TicTacToeGame {
 
-	private char[][] gameBoard = new char[3][3];
+	private static final int THREE = 3;
+	private char[][] gameBoard;
 
-	public char getSymbolAt(int row, int column) {
+	public TicTacToeGame() {
+		gameBoard = new char[THREE][THREE];
+	}
+
+	public char getSymbolAt(final int row, final int column) {
 		return gameBoard[row][column];
 	}
 
-	public void play(int row, int column, char player) {
+	public void play(final int row, final int column, final char player) {
 		gameBoard[row][column] = player;
 	}
 
